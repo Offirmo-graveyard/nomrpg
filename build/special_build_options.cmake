1bd05ce996fe4d0610b3225e0ec73b597ea5d955
Offirmo <offirmo.net@gmail.com>
2015-02-14 19:18:02 +0100

    old work

--- build/special_build_options.cmake
+++ build/special_build_options.cmake
@@ -4,9 +4,7 @@
 
 # option to disable Wt (when the lib is not available)
 SET( DEFAULT_USE_WT "ON" )
-if(${COMPUTER_NAME} STREQUAL "cmxd7095")
-	SET( DEFAULT_USE_WT  "OFF" )
-endif()
+#SET( DEFAULT_USE_WT  "OFF" )
 
 option(USE_WT  "(dev option) Activate the use of the Wt lib. Available : ON, OFF."  ${DEFAULT_USE_WT} )
 MARK_AS_ADVANCED( USE_WT )
